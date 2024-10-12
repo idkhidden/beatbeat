865847c88a9d1414ecb99ca25680b9331b4f14ff
idkhidden <91305428+idkhidden@users.noreply.github.com>
2024-10-13 02:02:45 +0500

    Update server.cpp

--- beatbeat/beatbeat-server/server.cpp
+++ beatbeat/beatbeat-server/server.cpp
@@ -19,7 +19,7 @@ void handleclient(SOCKET clientSocket)
     sockaddr_in clientAddr;
     int addrLen = sizeof(clientAddr);
     getpeername(clientSocket, (struct sockaddr*)&clientAddr, &addrLen);
-    cout << "[ beatbeat ] client connected\n";
+    cout << "[ beatbeat server ] client connected\n";
 
     random_device rd;
     mt19937 eng(rd());
@@ -30,7 +30,7 @@ void handleclient(SOCKET clientSocket)
         int randomNum = distr(eng);
         string msg = to_string(randomNum) + "\n";
         send(clientSocket, msg.c_str(), msg.size(), 0);
-        cout << "[ beatbeat ] heartbeat sent -> " << randomNum << endl;
+        cout << "[ beatbeat server ] heartbeat sent -> " << randomNum << endl;
 
         DWORD timeout = RECEIVE_TIMEOUT * 1000;
         setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
@@ -38,11 +38,11 @@ void handleclient(SOCKET clientSocket)
         int valRead = recv(clientSocket, buffer, sizeof(buffer), 0);
         if (valRead > 0)
         {
-            cout << "[ beatbeat ] heartbeat received -> " << buffer;
+            cout << "[ beatbeat server ] heartbeat received -> " << buffer;
         }
         else
         {
-            cout << "[ beatbeat ] failed to receive heartbeat\n";
+            cout << "[ beatbeat server ] failed to receive heartbeat\n";
             break; 
         }
 
@@ -66,14 +66,14 @@ int main()
 
     if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
     {
-        cout << "[ beatbeat ] winsocket failed\n";
+        cout << "[ beatbeat server ] winsocket failed\n";
         Sleep(2000);
         return 1;
     }
 
     if ((serversocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
     {
-        cout << "[ beatbeat ] socket creation failed\n";
+        cout << "[ beatbeat server ] socket creation failed\n";
         WSACleanup();
         Sleep(2000);
         return 1;
@@ -81,7 +81,7 @@ int main()
 
     if (bind(serversocket, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
     {
-        cout << "[ beatbeat ] bind failed\n";
+        cout << "[ beatbeat server ] bind failed\n";
         closesocket(serversocket);
         WSACleanup();
         Sleep(2000);
@@ -90,14 +90,14 @@ int main()
 
     if (listen(serversocket, 3) == SOCKET_ERROR)
     {
-        cout << "[ beatbeat ] listen failed\n";
+        cout << "[ beatbeat server ] listen failed\n";
         closesocket(serversocket);
         WSACleanup();
         Sleep(2000);
         return 1;
     }
 
-    cout << "[ beatbeat ] server listening -> " << PORT << endl;
+    cout << "[ beatbeat server ] server listening -> " << PORT << endl;
 
     while ((clientsocket = accept(serversocket, (struct sockaddr*)&client, &addrLen)) != INVALID_SOCKET)
     {
