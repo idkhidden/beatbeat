309109860ecf98ea8b85bc13d2f3b58584bf6f4b
idkhidden <91305428+idkhidden@users.noreply.github.com>
2024-10-13 02:02:24 +0500

    Update client.cpp

--- beatbeat/beatbeat-client/client.cpp
+++ beatbeat/beatbeat-client/client.cpp
@@ -23,21 +23,21 @@ int main()
 
     if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
     {
-        cout << "[ beatbeat ] winsocket failed\n";
+        cout << "[ beatbeat client ] winsocket failed\n";
         Sleep(2000);
         return 1;
     }
 
     if ((clientsocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
     {
-        cout << "[ beatbeat ] socket creation failed\n";
+        cout << "[ beatbeat client ] socket creation failed\n";
         WSACleanup();
         return 1;
     }
 
     if (inet_pton(AF_INET, "127.0.0.1", &server.sin_addr) <= 0)
     {
-        cout << "[ beatbeat ] invalid server\n";
+        cout << "[ beatbeat client ] invalid server\n";
         closesocket(clientsocket);
         WSACleanup();
         Sleep(2000);
@@ -46,7 +46,7 @@ int main()
 
     if (connect(clientsocket, (sockaddr*)&server, sizeof(server)) < 0)
     {
-        cout << "[ beatbeat ] connection failed\n";
+        cout << "[ beatbeat client ] connection failed\n";
         closesocket(clientsocket);
         WSACleanup();
         Sleep(2000);
@@ -57,24 +57,24 @@ int main()
     mt19937 eng(rd());
     uniform_int_distribution<> distr(1, 100);
 
-    cout << "[ beatbeat ] connected to server!\n";
+    cout << "[ beatbeat client ] connected to server!\n";
     while (true)
     {
         int heartbeat = recv(clientsocket, buffer, sizeof(buffer), 0);
         
         if (heartbeat > 0)
         {
-            cout << "[ beatbeat ] heartbeat received!\n";
+            cout << "[ beatbeat client ] heartbeat received!\n";
 
             int randomheartbeat = distr(eng);
             string response = to_string(randomheartbeat) + "\n";
 
             send(clientsocket, response.c_str(), response.size(), 0);
-            cout << "[ beatbeat ] heartbeat sent!\n";
+            cout << "[ beatbeat client ] heartbeat sent!\n";
         }
         else
         {
-            cout << "[ beatbeat ] server disconnected\n";
+            cout << "[ beatbeat client ] server disconnected\n";
             Sleep(2000);
             return 1;
         }
